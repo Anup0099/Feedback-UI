98480280dc2b787777b17612426effa507266443
anup <anuppal602@gmail.com>
2022-01-14 20:15:55 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+const Header = ({ text, bgColor, textColor }) => {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h1>{text}</h1>
+      </div>
+    </header>
+  );
+};
+Header.defaultProps = {
+  text: "Material UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+};
+
+export default Header;
