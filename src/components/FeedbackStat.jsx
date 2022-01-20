5555ecd5685e0e4dfd102744d9c8a5000ea0ac23
anup <anuppal602@gmail.com>
2022-01-21 01:15:58 +0530

    forms will start

--- src/components/FeedbackStat.jsx
+++ src/components/FeedbackStat.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const FeedbackStat = ({feedback}) => {
-  return <div className='feedback-stats'>
-      <h4>{feedback.length}</h4>
-  </div>;
+const FeedbackStat = ({ feedback }) => {
+  let average =
+    feedback.reduce((acc, curr) => {
+      return acc + curr.rating;
+    }, 0) / feedback.length;
+    average=Math.round(average * 100) / 100;
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length}Reviews</h4>
+      <h4>Average rating:{isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
 };
 
 export default FeedbackStat;
